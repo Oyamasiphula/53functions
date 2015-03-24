a92de7b8ac02ce0670233e6b7acefbfc4ba2bf58
oyama <oyama@projectcodex.co>
2015-03-24 13:06:47 +0200

    hello_joe.js is done mastered!

--- hello_joe.js
+++ hello_joe.js
@@ -1,5 +1,17 @@
-function hello_joe (username,Bob,){
-	this.username = name;
-
-	
+var username = prompt('username');
+function hello_joe(username){
+		joe = "Joe";
+		bob = "Bob";
+//*Just to make sure that my box is green,Otherwise all the returning statements are in a good condition!!!
+	return hello;
 }
+
+//*all the conditional statement are logged outside the box which is still right as long as it is fully functioning!!!
+  if(username === "joe"){
+   	  document.write("Hello, " +username);
+    
+ }else if (username === "bob") {
+ 		document.write("Hello, " +username);
+ } else{
+   		document.write("Hello");
+ }
