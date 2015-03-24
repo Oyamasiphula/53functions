26977cb368f87ef04ac97dd4b4966a4f69fcc2c4
oyama <oyama@projectcodex.co>
2015-03-24 13:07:35 +0200

    hello_joe_test.js is done mastered!

--- hello_joe_test.js
+++ hello_joe_test.js
@@ -1,5 +1,5 @@
-TestMyFunction.run("test hello joe function",function(assert){
-		var result = "hello_joe()";
+TestMyCode.run("test hello joe function",function(assert){
+		var result = hello_joe();
 		//is the result as we expected?
 		assert.equals("Hello",result);
 
