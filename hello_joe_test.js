d7e93cc3ae7ed3dff644e35b885663e2f21ff09f
oyama <oyama@projectcodex.co>
2015-03-25 16:09:08 +0200

    hello_joe_test.js is updated!

--- hello_joe_test.js
+++ hello_joe_test.js
@@ -1,6 +1,18 @@
 TestMyCode.run("test hello joe function",function(assert){
-		var result = hello_joe();
+		var result = hello_joe('joe');
 		//is the result as we expected?
 		assert.equals("Hello",result);
+
+});
+TestMyCode.run("test hello joe function",function(assert){
+		var result = hello_joe('bob');
+		//is the result as we expected?
+		assert.equals("Hello",result);
+
+});
+TestMyCode.run("test hello joe function",function(assert){
+		var result = hello_joe('oyama');
+		//is the result as we expected?
+		assert.equals("Hello, oyama",result);
 
 });
