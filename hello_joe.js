121ebb16c5162e7a5f212853e7d141852f1559dd
oyama <oyama@projectcodex.co>
2015-03-25 16:08:44 +0200

    hello_joe.js is updated!

--- hello_joe.js
+++ hello_joe.js
@@ -1,17 +1,10 @@
-var username = prompt('username');
 function hello_joe(username){
-		joe = "Joe";
-		bob = "Bob";
-//*Just to make sure that my box is green,Otherwise all the returning statements are in a good condition!!!
-	return hello;
-}
+	
 
-//*all the conditional statement are logged outside the box which is still right as long as it is fully functioning!!!
-  if(username === "joe"){
-   	  document.write("Hello, " +username);
+  if(username === "joe" || username === "bob" ){
+   	  return("Hello");
     
- }else if (username === "bob") {
- 		document.write("Hello, " +username);
- } else{
-   		document.write("Hello");
- }
+ }else{
+ 	return("Hello," +" "+username);
+ 	}
+}
